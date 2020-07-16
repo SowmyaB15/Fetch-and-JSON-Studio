1adde427b0496dbf4808ada0775b61c50d171dd9
Sowmya Bhadraradhya <sowmyab2010@gmail.com>
2020-07-15 21:03:44 -0500

    script.js is modified to display astronauts list

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            let container = document.getElementById("container");
+            // let astronautList = '';
+            let astronautList = `<h3>Astronauts Count: ${json.length}</h3>`; // bonus
+            for (astronaut of json) {
+                astronautList +=  
+                `           
+                    <div class="astronaut">
+                            <div class="bio">
+                                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                                <ul>
+                                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                    <li>Active: ${astronaut.active}</li>
+                                    <li>Skills: ${astronaut.skills}</li>
+                                </ul>
+                            </div>
+                            <img class="avatar" src= ${astronaut.picture}></img>
+                    </div>
+                `;
+            }
+            container.innerHTML = astronautList;
+        });
+    });
+});
